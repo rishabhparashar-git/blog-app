54c4a8fc085d76c59eb07c72740f106c1e6893bb
Rishabh Parashar <parashar040601@gmail.com>
2023-01-28 15:59:18 +0530

    updated imports

--- app/(admin)/studio/[[...index]]/page.tsx
+++ app/(admin)/studio/[[...index]]/page.tsx
@@ -2,7 +2,7 @@
 
 import { NextStudio } from "next-sanity/studio";
 
-import config from "../../../sanity.config"; //sanity config file
+import config from "../../../../sanity.config"; //sanity config file
 
 export default function StudioPage() {
   //  Supports the same props as `import {Studio} from 'sanity'`, `config` is required
