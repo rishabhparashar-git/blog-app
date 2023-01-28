f9f73ed92d00c40d6876ed8536a7627617ef3112
Rishabh Parashar <parashar040601@gmail.com>
2023-01-28 14:29:30 +0530

    sanity.io set up

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,11 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "e1a7xba7"; // the ! states to the ts that this variable will always contain value and never be null
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineCliConfig({
   api: {
-    projectId: 'e1a7xba7',
-    dataset: 'production'
-  }
-})
+    projectId,
+    dataset,
+  },
+});
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,22 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "e1a7xba7"; // the ! states to the ts that this variable will always contain value and never be null
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Sanity Blog CMS',
+  basepath: "/studio",
+  name: "RISHABH_CONTENT_STUDIO",
+  title: "Sanity Blog CMS",
 
-  projectId: 'e1a7xba7',
-  dataset: 'production',
+  projectId,
+  dataset,
 
   plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});
